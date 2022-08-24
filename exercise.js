92e99814598af27f5fbbdd0ed91b700cd16a1209
seiviel14 <senriquevelasquez@gmail.com>
2022-08-24 15:53:01 +0200

    Exercise completed

---
+++ exercise.js
@@ -0,0 +1,14 @@
+let integer_height = 173;
+let float_height = 1.73;
+let float_weight = 86.5;
+let up_rounded_height = Math.round(float_height);
+let down_rounded_weight = Math.floor(float_weight);
+let max_number = Number.MAX_VALUE;
+let equal = (max_number == "el máximo valor que se puede obtener en Javascript + 1")
+
+console.log(integer_height);
+console.log(float_height);
+console.log(float_weight);
+console.log(up_rounded_height);
+console.log(down_rounded_weight);
+console.log(equal);
